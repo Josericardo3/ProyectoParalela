e52924a5916328e8cfa8be479f383f5728d33338
ff <dd>
2021-06-26 03:30:13 -0500

    add deploy

--- app.js
+++ app.js
@@ -1,46 +1,81 @@
 'use strict';
 
 const express = require('express');
-const {OAuth2Client} = require('google-auth-library');
+//const {OAuth2Client} = require('google-auth-library');
 const path = require('path');
-const process = require('process'); 
-const {PubSub} = require('@google-cloud/pubsub');
+const process = require('process');
+const bodyParser = require('body-parser');
+//const {PubSub} = require('@google-cloud/pubsub');
 
-const authClient = new OAuth2Client();
-const pubsub = new PubSub();
+//const authClient = new OAuth2Client();
+//const pubsub = new PubSub();
 
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json({ limit: '50mb' }));
 
-const formBodyParser = express.urlencoded({extended: false});
-const jsonBodyParser = express.json();
 const messages = [];
 const claims = [];
 const tokens = [];
-const {PUBSUB_VERIFICATION_TOKEN} = process.env;
+/*const {PUBSUB_VERIFICATION_TOKEN} = process.env;
 const TOPIC = "newtopic"//process.env.PUBSUB_TOPIC;
+const pubSubClient = new PubSub();
 
 const topic = pubsub.topic(TOPIC);
+
+const listenForMessages = () => {
+  const subscription = pubSubClient.subscription("sucriptorextractor");
+
+  // Create an event handler to handle messages
+  let messageCount = 0;
+
+  const messageHandler = message => {
+    console.log(`Received message ${message.id}:`);
+    console.log(`\tData: ${message.data}`);
+    console.log(`\tAttributes: ${message.attributes}`);
+    const messagebuffer = Buffer.from(message.data, 'base64').toString(
+      'utf-8'
+    );
+    messages.push(messagebuffer)
+    // "Ack" (acknowledge receipt of) the message
+    message.ack();
+  };
+
+  // Listen for new message until timeout is hit
+  subscription.on('message', messageHandler);
+
+  const timeout = 600
+
+  setTimeout(() => {
+    subscription.removeListener('message', messageHandler);
+    console.log(`${messages.length} message(s) received.`);
+  }, timeout * 1000);
+}
+*/
 
 app.get('/', (req, res) => {
   res.render('index', {messages, tokens, claims});
 });
-app.get('/detalles', (req, res) => {
-  res.render('detalles');
-});
 app.get('/trabajador', (req, res) => {
-  res.render('trabajador');
+  res.render('trabajador', {messages, tokens, claims});
 });
-app.post('/', formBodyParser, async (req, res, next) => {
-  if (!req.body.payload) {
-    res.status(400).send('Missing payload');
-    return;
-  }
+app.get('/detalles', (req, res) => {
+  res.render('detalles', {messages, tokens, claims});
+});
 
-  const data = Buffer.from(req.body.payload);
+/*
+app.post('/envio', async (req, res, next) => {
   try {
+    if (!req.body.msjstring) {
+      res.status(400).send('Missing message');
+      return;
+    }
+
+    const data = Buffer.from(req.body.msjstring);
+    
     const messageId = await topic.publish(data);
     res.status(200).send(`Message ${messageId} sent.`);
   } catch (error) {
@@ -48,7 +83,7 @@ app.post('/', formBodyParser, async (req, res, next) => {
   }
 });
 
-app.post('/pubsub/push', jsonBodyParser, (req, res) => {
+app.post('/pubsub/push', (req, res) => {
   if (req.query.token !== PUBSUB_VERIFICATION_TOKEN) {
     res.status(400).send();
     return;
@@ -63,40 +98,41 @@ app.post('/pubsub/push', jsonBodyParser, (req, res) => {
   res.status(200).send();
 });
 
-app.post('/pubsub/authenticated-push', jsonBodyParser, async (req, res) => {
-  if (req.query.token !== PUBSUB_VERIFICATION_TOKEN) {
-    res.status(400).send('Invalid request');
-    return;
-  }
-
+app.post('/pubsub/authenticated-push', async (req, res) => {
   try {
+    if (req.query.token !== PUBSUB_VERIFICATION_TOKEN) {
+      res.status(400).send('Invalid request');
+      return;
+    }
+
     const bearer = req.header('Authorization');
     const [, token] = bearer.match(/Bearer (.*)/);
     tokens.push(token);
 
     const ticket = await authClient.verifyIdToken({
-      idToken: token,
-      audience: 'example.com',
+      idToken: 'h7rVohARqS1Qn6SykVTTbVLp',
+      audience: '754073496994-488l1lak1803m5uf7749v4hacbk7rufe.apps.googleusercontent.com',
     });
 
     const claim = ticket.getPayload();
 
     claims.push(claim);
+
+    const message = Buffer.from(req.body.message.data, 'base64').toString(
+      'utf-8'
+    );
+
+    messages.push(message);
+
+    res.status(200).send();
   } catch (e) {
     res.status(400).send('Invalid token');
     return;
   }
-
-  const message = Buffer.from(req.body.message.data, 'base64').toString(
-    'utf-8'
-  );
-
-  messages.push(message);
-
-  res.status(200).send();
 });
+*/
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
